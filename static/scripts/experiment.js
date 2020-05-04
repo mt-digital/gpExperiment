99fc7cc0da24b1fa800b37cf17e05905eb62b7ec
Matthew Turner <maturner01@gmail.com>
2020-05-04 00:05:14 -0700

    new workflow pre-question before chatroom; next adding post-discussion question

--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -1,18 +1,51 @@
 /*globals $, dallinger */
-var my_node_id;
+
+var MY_NODE_ID;
+
+/**
+ * Get current transmissions for this agent and initiate an infinite recursive
+ * loop that listens for new transmissions by checking the server every 100ms
+ * for new transmissions.
+ *
+ * As other agents chat, this turns into Info submissions that are then
+ * transmitted along Vectors to neighboring Agents. Here the script is
+ * collecting all new transmissions from all other agents connected to this
+ * agent. A transmission is "new" if it has been submitted to the server
+ * since the last time this client requested its transmissions via this
+ * getTransmissions call.
+ */
+var loadTransmissions = function(MY_NODE_ID) {
+
+  // Fetch 'pending' transmissions that have not yet been requested.
+  dallinger.getTransmissions(MY_NODE_ID, { status: 'pending' })
+    .done(resp => {
+      // Iterate over transmissions received, display each one.
+      resp.transmissions.forEach(t => displayTransmission(t.info_id));
+
+      // Repeat request-display loop every 100ms.
+      setTimeout(() => loadTransmissions(MY_NODE_ID), 100);
+    });
+};
+
 
 // Create the agent. Called once in experiment.html.
-var create_agent = function () {
+var createAgent = function() {
+
   dallinger.createAgent()
-    .done(function (resp) {
-      my_node_id = resp.node.id;
-      console.log(my_node_id);
+    .done(resp => {
+
       $("#stimulus").show();
+
       $("#response-form").show();
+
       $("#send-message").removeClass("disabled");
       $("#send-message").html("Send");
+
       $("#reproduction").focus();
-      get_transmissions(my_node_id);
+
+      MY_NODE_ID = resp.node.id;
+
+      loadTransmissions(MY_NODE_ID);
     })
     .fail(function (rejection) {
       // A 403 is our signal that it's time to go to the questionnaire
@@ -26,42 +59,30 @@ var create_agent = function () {
     });
 };
 
-var get_transmissions = function (my_node_id) {
-  dallinger.getTransmissions(my_node_id, { status: 'pending' })
-    .done(function (resp) {
-      console.log(resp);
-      transmissions = resp.transmissions;
-      for (var i = transmissions.length - 1; i >= 0; i--) {
-        console.log(transmissions[i]);
-        display_info(transmissions[i].info_id);
-      }
-      setTimeout(function () { get_transmissions(my_node_id); }, 100);
-    });
-};
 
-var display_info = function(info_id) {
-  dallinger.getInfo(my_node_id, info_id)
+
+var displayTransmission = function(info_id) {
+  dallinger.getInfo(MY_NODE_ID, info_id)
     .done(function (resp) {
       console.log(resp.info.contents);
       $("#story").append("<p>" + resp.info.origin_id + ': ' + resp.info.contents + "</p>");
     });
 };
 
-var send_message = function() {
+
+var sendMessage = function() {
 
   $("#send-message").addClass("disabled");
   $("#send-message").html("Sending...");
 
-  response = $("#reproduction").val();
+  var response = $("#reproduction").val();
   $("#reproduction").val("");
-
-  var myId = dallinger.identity.participantId;
 
   $("#story").append("<p style='color: #1693A5;'>" + "Me: " + response + "</p>");
   $("#reproduction").focus();
 
   // Create a new Info object based on latest statement and POST it to server.
-  dallinger.createInfo(my_node_id, {
+  dallinger.createInfo(MY_NODE_ID, {
     contents: response,
     info_type: "Info"
   }).done(function (resp) {
@@ -71,9 +92,11 @@ var send_message = function() {
   });
 };
 
-var leave_chatroom = function() {
+
+var leaveChatroom = function() {
   dallinger.goToPage("questionnaire");
 };
+
 
 $(document).keypress(function (e) {
   if (e.which === 13) {
@@ -83,21 +106,22 @@ $(document).keypress(function (e) {
   }
 });
 
+
 $(document).ready(function() {
-
-  // Send a message.
-  $("#send-message").click(function() {
-    send_message();
-  });
-
-  // Leave the chatroom.
-  $("#leave-chat").click(function() {
-    leave_chatroom();
-  });
-
-  // Proceed to the waiting room.
+    
+  // Proceed to the waiting room listener.
   $("#go-to-waiting-room").click(function() {
     dallinger.goToPage("waiting");
   });
 
+  // Message send listener.
+  $("#send-message").click(function() {
+    sendMessage();
+  });
+
+  // Leave room listener.
+  $("#leave-chat").click(function() {
+    leaveChatroom();
+  });
 });
+
--- static/scripts/survey_question.js
+++ static/scripts/survey_question.js
@@ -1,16 +1,57 @@
+/*globals $, dallinger */
+
+var NUM_PARTS_EXCEEDED_MESSAGE = '<p>The experiment has exceeded the maximum number of participants, your participation is not required. Click the button below to complete the HIT. You will be compensated as if you had completed the task.</p><button type="button" class="button btn-success">Complete</button>';
+
 $(document).ready(function() {
-
-  $("#submit-answer").click(() => 
-    {
-      // Post to question route, creating a question response for this
-      // participant.
-    }
-  );
-
-  var potential_questions = [
+  var potentialQuestions = [
     "How many legs does a spider have?",
     "How many drops of water in the ocean?"
   ];
 
-  $("#question").html(potential_questions[0]);
+  var question = potentialQuestions[0];
+
+  $("#question").html(question);
+
+  $("#submit-answer").click(() => {
+
+    // Post to question route, creating a question response for this
+    // participant.
+    var answerValue = $("input[name='response']:checked").val(); 
+
+    console.log(answerValue);
+    
+    var urlParams = new URLSearchParams(window.location.search);
+
+    var questionNumber = urlParams.get("number");
+    // 'pre' or 'post', as in position relative to the opinion exchange step.
+    var questionPosition = urlParams.get("position");
+
+    console.log(questionNumber)
+    console.log(questionPosition)
+      
+    // Now ready to create a participant, which must be done before we
+    // can post this participant's answer to the server, if we even need
+    // them for the opinion exchange.
+    $("#progress-container").show();
+
+    dallinger.createParticipant().done(() => {
+      if (dallinger.skip_experiment) {
+        dallinger.allowExit();
+        $('.main_div').html(NUM_PARTS_EXCEEDED_MESSAGE);
+
+        $('#not-needed-exit').click(dallinger.submitAssignment);
+      } else {
+        // Post response to question and proceed to opinion exchange, currently
+        // called by its to-be-replaced placeholder 'experiment'.
+        dallinger.post("/question/" + dallinger.identity.participantId, 
+          {
+            question: question + "-" + questionPosition,
+            number: questionNumber,
+            response: answerValue
+          }
+        ).done(() => dallinger.goToPage('experiment'));
+      }
+    });
+  });
+
 });
